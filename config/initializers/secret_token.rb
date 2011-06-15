46f9060849d2fe97e09773114ca2d6b3734e8f17
Sumeet Jain <sumeet@sumeetjain.com>
2011-06-14 17:29:58 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocalJudo::Application.config.secret_token = '36d8fb9f3ac8d1233a2548e7144d0d06580ac5d78357ae130132d046987c053285ae2c4b1fb9eb059099df9e0642ecb828550598645b5ac985b15b80b7d84b40'
